619046f8c91d22b5367d40a4b9bf22f38b311138
jlryan29 <jlryan29.jr@gmail.com>
2021-04-05 14:35:58 -0400

    Delete opensc source tar prior to changing into opensc build directory to avoid syntax errors

--- linux-cacsupport-base.sh
+++ linux-cacsupport-base.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 function install_prereq {
 	sudo apt-get install pcscd libccid libpcsclite-dev libssl-dev libreadline-dev autoconf automake build-essential docbook-xsl xsltproc libtool pkg-config zlib1g-dev libnss3-tools libpam-pkcs11 --assume-yes
 }
@@ -9,7 +8,8 @@ function build_opensc {
 	cd $HOME/Downloads
 	wget https://github.com/OpenSC/OpenSC/releases/download/0.21.0/opensc-0.21.0.tar.gz
 	tar xfvz opensc-*.tar.gz
-	cd $HOME/Downloads/opensc-*
+	rm opensc-*.tar.gz
+	cd $HOME/Downloads/opensc-0*
 	./bootstrap
 	./configure --prefix=/usr --sysconfdir=/etc/opensc
 	make
