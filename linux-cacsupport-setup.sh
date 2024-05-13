85331067511b9fc3d38be0cf2fddac4bb10be87e
George <drpresq@gmail.com>
2024-05-13 13:28:45 -0400

    Added back DoD Certificate Installation (removed in version 1.0.1); Included logic to gracefully handle the existing installtion of OpenSC and Security Hardware modules in the default PKI security database.

--- linux-cacsupport-setup.sh
+++ linux-cacsupport-setup.sh
@@ -16,6 +16,13 @@ function install_prereq {
 	sudo apt-get install pcscd libccid libpcsclite-dev libssl-dev libreadline-dev autoconf automake build-essential docbook-xsl xsltproc libtool pkg-config zlib1g-dev libnss3-tools libpam-pkcs11 --assume-yes > $QUIET 2>&1
 }
 
+function download_certs {
+	cd "$HOME"/Downloads || exit
+	wget https://militarycac.com/maccerts/AllCerts.zip
+	mkdir dodCerts
+	unzip AllCerts.zip -d ./dodCerts
+}
+
 function build_opensc {
 	cd "$HOME"/Downloads || exit
 	wget https://github.com/OpenSC/OpenSC/releases/download/$VERSION/opensc-$VERSION.tar.gz > $QUIET 2>&1
@@ -32,6 +39,10 @@ function clean_opensc {
 	rm -rf "$HOME"/Downloads/opensc-*
 }
 
+function clean_certs {
+  rm -rf "$HOME"/Downloads/dodCerts
+}
+
 function print_message {
 	ENDCOLOR="\033[0m"
 	YELLOW_BACK="\033[1;43m"/home/user
@@ -44,20 +55,37 @@ function print_message {
 #	Main
 ###
 
-print_message "Installing Prerequisites"
-install_prereq
+print_message "Downloading DoD Root Certificates"
+download_certs
 
-print_message "Building and installing from source"
-build_opensc
+if ! (find / -type f -name opensc-pkcs11.so 2>$QUIET | grep opensc > $QUIET 2>&1); then
+  print_message "No existing OpenSC installation found - Building from Source"
+  print_message "Installing Prerequisites"
+  install_prereq
 
-print_message "Cleaning up installation files"
-clean_opensc
+  print_message "Building and installing from source"
+  build_opensc
 
-print_message "Adding device to Firefox security database"
-modutil -dbdir "$(find "$HOME"/.mozilla/firefox/ -iname '*.default-release')" -add "\"$MIDDLEWARE $VERSION\"" -libfile "$(find /lib/ -type f -name opensc-pkcs11.so)"
+  print_message "Cleaning up installation files"
+  clean_opensc
+fi
 
-print_message "Adding device to default PKI security database"
-modutil -dbdir sql:"$HOME"/.pki/nssdb/ -add "\"$MIDDLEWARE $VERSION\"" -libfile "$(find /lib/ -type f -name opensc-pkcs11.so)"
+if (which firefox 2>$QUIET | grep ".*" > $QUIET 2>&1); then
+  print_message "Updating the Firefox security database"
+  modutil -dbdir "$(find "$HOME"/.mozilla/firefox/ -iname '*.default-release')" -add "\"$MIDDLEWARE $VERSION\"" -libfile "$(find /lib/ -type f -name opensc-pkcs11.so)"
+
+  print_message "Installing Root Certificates in Firefox security database"
+  cd dodCerts || exit
+	for value in "$HOME"/Downloads/dodCerts/*; do s=${value##*/}; certutil -A -i "$value" -n "${s%.cer} - U.S. Government" -t"CT,C" -d "$(find "$HOME"/.mozilla/firefox -iname "*.default-release")"; done
+fi
+
+print_message "Updating the default PKI security database"
+if ! (which p11-kit > $QUIET 2>&1 && p11-kit list-modules | grep "module: opensc" > $QUIET 2>&1); then
+  modutil -dbdir sql:"$HOME"/.pki/nssdb/ -add "\"$MIDDLEWARE $VERSION\"" -libfile "$(find / -type f -name opensc-pkcs11.so 2>$QUIET)"
+fi
+for value in "$HOME"/Downloads/dodCerts/*; do s=${value##*/}; certutil -A -i "$value" -n "${s%.cer} - U.S. Government" -t"CT,C" -d "$HOME"/.pki/nssdb; done
+
+print_message "Cleaning up DoD Root Certificate Files"
+clean_certs
 
 print_message "Installation Complete! - You are now ready to use a PIV II Smartcard"
-
